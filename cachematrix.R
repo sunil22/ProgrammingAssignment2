e881da37ae5052730a8131f932b8985956fe1d71
sunil22 <sunil.kumar.nitie@gmail.com>
2015-03-06 00:17:27 +0530

    Calculate the inverse of matrix by SUNIL

--- cachematrix.R
+++ cachematrix.R
@@ -18,66 +18,20 @@ getinverse <- function() inverse
 list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-## Write a short comment describing this function
-## cacheSolve uses corpcor, a library that avoids determinants and uses orthogonal descomposition
-## note: this function will try to load corpcor library and if it's not installed will try to install the library
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
 
-cacheSolve <- function(X, ...) 
-{
-if(require("corpcor")){
-	print("corpcor is loaded correctly")
-	} else {
-		print("trying to install corpcor")
-		install.packages("corpcor")
-		if(require(corpcor)){
-			print("corpcor installed and loaded")
-			} else {
-			stop("could not install corpcor")
-			}
-		}
-inverse <- X$getinverse()
-if(!is.null(inverse)){
-	message("matrix is in memory")
-	return(inverse)
-	}
-message("inverse is not in memory so the inverse (if exist) is gonna be computed")
-data <- X$get()
-inverse <- pseudoinverse(data, ...)
-X$setinverse(inverse)
-inverse
+# This function assumes that the matrix is always invertible.
+cacheSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
-
-
-#Experiment to try if it works
-#square matrix
-X <- matrix(rpois(25,3), nrow = 5)
-cX <- makeCacheMatrix(X)
-cX$get()
-cacheSolve(cX)
-cacheSolve(cX)
-invX <- cacheSolve(cX)
-
-#Experiment to try if it works
-#rectangular matrix rows > cols
-Y <- matrix(rpois(20,2), nrow = 5, ncol = 4)
-cY <- makeCacheMatrix(Y)
-cY$get()
-cacheSolve(cY)
-cacheSolve(cY)
-invY <- cacheSolve(cY)
-
-#Experiment to try if it works
-#rectangular matrix rows < cols
-Z <- matrix(rpois(20,1), nrow = 4, ncol = 5)
-cZ <- makeCacheMatrix(Z)
-cZ$get()
-cacheSolve(cZ)
-cacheSolve(cZ)
-invZ <- cacheSolve(cZ)
-
-#Experiment to try if it works
-#multiplication must return identity or closer
-invX %*% X 
-X %*% invX
-invY %*% Y 
-Z %*% invZ 
